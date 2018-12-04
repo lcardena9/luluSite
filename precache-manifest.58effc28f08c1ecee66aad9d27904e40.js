e2525824f4895139d5a89a97afb40144e16f8263
lcardena9 <42814907+lcardena9@users.noreply.github.com>
2018-12-03 19:38:39 -0800

    first upload

---
+++ precache-manifest.58effc28f08c1ecee66aad9d27904e40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e4c857c2ac6c21c7a0cd06fb9424cb",
+    "url": "/static/media/video.13e4c857.mp4"
+  },
+  {
+    "revision": "c3e80b2448cf9ece3a7441f9789c48d5",
+    "url": "/static/media/lucasLogo.c3e80b24.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ae6cff6e26d24d21da2",
+    "url": "/static/js/main.0ae6cff6.chunk.js"
+  },
+  {
+    "revision": "9fb3af666c02bcc4cbb0",
+    "url": "/static/js/1.9fb3af66.chunk.js"
+  },
+  {
+    "revision": "0ae6cff6e26d24d21da2",
+    "url": "/static/css/main.9ac19922.chunk.css"
+  },
+  {
+    "revision": "ac3a5145a07d26c00608a3fb98adff1c",
+    "url": "/index.html"
+  }
+];
